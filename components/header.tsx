e8e58a7382d8897baa33c44d0c7f204b17931cec
RamziBach <ramzibach.dev@gmail.com>
2025-10-03 09:26:49 -0400

    brand icons

--- components/header.tsx
+++ components/header.tsx
@@ -1,11 +1,43 @@
+import { Button } from '@/components/ui/button';
+import { Separator } from '@/components/ui/separator';
+
 import ModeToggle from '@/components/mode-toggle';
+
+import { SiGithub } from '@icons-pack/react-simple-icons';
+import { SiYoutube } from '@icons-pack/react-simple-icons';
 
 export default function Header() {
   return (
     <header className="max-w-xl m-auto px-4">
-      <div className="flex justify-end mt-1">
+      <nav className="flex justify-end items-center gap-1 mt-1">
+        <Button variant="ghost" size="icon" asChild>
+          <a
+            href="https://github.com/RamziBach"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="GitHub"
+          >
+            <SiGithub />
+          </a>
+        </Button>
+        <div className="h-5">
+          <Separator orientation="vertical" />
+        </div>
+        <Button variant="ghost" size="icon" asChild>
+          <a
+            href="https://youtube.com/@slayingthedragon"
+            target="_blank"
+            rel="noopener noreferrer"
+            title="YouTube"
+          >
+            <SiYoutube />
+          </a>
+        </Button>
+        <div className="h-5">
+          <Separator orientation="vertical" />
+        </div>
         <ModeToggle />
-      </div>
+      </nav>
     </header>
   );
 }
