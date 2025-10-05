d6b81a1387f936eae00576f95f30bf51d749cc34
RamziBach <ramzibach.dev@gmail.com>
2025-10-04 21:31:02 -0400

    added codepen to header and refactored code

--- components/header.tsx
+++ components/header.tsx
@@ -3,36 +3,52 @@ import { Separator } from '@/components/ui/separator';
 
 import ModeToggle from '@/components/mode-toggle';
 
-import { SiGithub } from '@icons-pack/react-simple-icons';
-import { SiYoutube } from '@icons-pack/react-simple-icons';
+import { SiGithub, SiYoutube, SiCodepen } from '@icons-pack/react-simple-icons';
+
+const socialLinks = [
+  {
+    id: 'github',
+    href: 'https://github.com/RamziBach',
+    title: 'GitHub',
+    icon: SiGithub,
+  },
+  {
+    id: 'codepen',
+    href: 'https://codepen.io/ramzibach-the-styleful',
+    title: 'CodePen',
+    icon: SiCodepen,
+  },
+  {
+    id: 'youtube',
+    href: 'https://youtube.com/@slayingthedragon',
+    title: 'YouTube',
+    icon: SiYoutube,
+  },
+];
 
 export default function Header() {
   return (
     <header className="max-w-xl m-auto px-4">
       <nav className="flex justify-end items-center gap-1 mt-1">
-        <Button variant="ghost" size="icon" asChild>
-          <a
-            href="https://github.com/RamziBach"
-            target="_blank"
-            rel="noopener noreferrer"
-            title="GitHub"
-          >
-            <SiGithub />
-          </a>
-        </Button>
-        <div className="h-5">
-          <Separator orientation="vertical" />
-        </div>
-        <Button variant="ghost" size="icon" asChild>
-          <a
-            href="https://youtube.com/@slayingthedragon"
-            target="_blank"
-            rel="noopener noreferrer"
-            title="YouTube"
-          >
-            <SiYoutube />
-          </a>
-        </Button>
+        {socialLinks.map((link, index) => (
+          <div key={link.id} className="contents">
+            <Button variant="ghost" size="icon" asChild>
+              <a
+                href={link.href}
+                target="_blank"
+                rel="noopener noreferrer"
+                title={link.title}
+              >
+                <link.icon />
+              </a>
+            </Button>
+            {index < socialLinks.length - 1 && (
+              <div className="h-5">
+                <Separator orientation="vertical" />
+              </div>
+            )}
+          </div>
+        ))}
         <div className="h-5">
           <Separator orientation="vertical" />
         </div>
